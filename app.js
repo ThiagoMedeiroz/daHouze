18f56bc7748ab4dd39a4332cab7547a394264ac2
JoseCedrim <jose.cedrim@hotmail.com>
2022-05-08 21:05:03 -0300

    Pag de login/cadastro

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.render('loginCadastro')
+  })
+  
+  app.post('/', (req, res) => {
+    // const users = getInfoDatabase('users') CRIAR DATABASE
+    const { email, password } = req.body
+  
+    const userExists = users.find(user => {
+      return user.email === email && user.password === password
+    }) 
+})
+  
+   
 
 
 
