eb692a6eece2ae642d6a28d338792af8504dbd45
JoseCedrim <jose.cedrim@hotmail.com>
2022-05-18 18:04:36 -0300

    colocando pastas novas.

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const path = require('path');
 const app = express();
 
 
+
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 
-
 app.get('/', (req, res) => {
-    res.render('loginCadastro')
+    res.render('home')
   })
   
   app.post('/', (req, res) => {
