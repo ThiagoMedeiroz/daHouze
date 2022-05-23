64db0a2a9e817639aaf8e80b97d0f67095fde0c2
JoseCedrim <jose.cedrim@hotmail.com>
2022-05-23 10:47:26 -0300

    Fazendo rotas

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const loginRouter = require("./routers/Login");
+const homeRouter = require("./routers/Home");
 
 
 
@@ -10,19 +11,20 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.render('home')
-  })
+app.use(loginRouter)
+app.use(homeRouter)
+// app.get('/', (req, res) => {
+//     res.render('home')
+//   })
   
-  app.post('/', (req, res) => {
-    // const users = getInfoDatabase('users') CRIAR DATABASE
-    const { email, password } = req.body
+//   app.post('/', (req, res) => {
+//     // const users = getInfoDatabase('users') CRIAR DATABASE
+//     const { email, password } = req.body
   
-    const userExists = users.find(user => {
-      return user.email === email && user.password === password
-    }) 
-})
+//     const userExists = users.find(user => {
+//       return user.email === email && user.password === password
+//     }) 
+// })
   
    
 
